f0b637538d16699ae1464b9a73c6457c92c6aa7e
Sami Guntepe <sguntepe@k2m02s08.42kocaeli.com.tr>
2022-12-15 13:35:51 +0300

    42

--- get_next_line.c
+++ get_next_line.c
@@ -1,33 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguntepe <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 13:31:47 by sguntepe          #+#    #+#             */
+/*   Updated: 2022/12/15 13:31:48 by sguntepe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-#include <stdio.h>
-char *get_next_line(int fd)
+
+char	*ft_read_to_left_str(int fd, char *left_str)
 {
-    int file;
-    char *filename = "data.txt";
-    static char *data;
-    int i;
-    int bytesRead;
-    file = open(filename,O_RDONLY);
-    if(BUFFER_SIZE < 0)
-        return (NULL);
-    if(file < 0)
-        return (NULL);
-    data = malloc(BUFFER_SIZE * sizeof(char));
-    i = 0;
-    while(read(fd, data, BUFFER_SIZE) > 0)
-    {
-        printf("%s",data);
-    }
-    
-    return(data);
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
+	}
+	free(buff);
+	return (left_str);
 }
 
-int main()
+char	*get_next_line(int fd)
 {
-    int file;
-    file = open("data.txt",O_RDONLY);
-    get_next_line(file);
-    
-    
-    close(file);
+	char		*line;
+	static char	*left_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	left_str = ft_read_to_left_str(fd, left_str);
+	if (!left_str)
+		return (NULL);
+	line = ft_get_line(left_str);
+	left_str = ft_new_left_str(left_str);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,15 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguntepe <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 13:31:45 by sguntepe          #+#    #+#             */
+/*   Updated: 2022/12/15 13:31:46 by sguntepe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 4
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
 
-char *get_next_line(int fd);
-int count_line(char *data);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+char	*get_next_line(int fd);
 
 #endif
